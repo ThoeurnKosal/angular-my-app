bf411e98cffbf9c9fc55a6fdc9432505582743bd
Kosal Mr. Thoeurn <Kosal@udaya.lan>
2018-10-22 17:28:26 +0700

    initail commit my-first app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Mr. Nuruto'},
+    {id: 13, name: 'Mr. Hatoya'},
+    {id: 14, name: 'Mr. Java'},
+    {id: 15, name: 'Mr. Angular'},
+    {id: 16, name: 'Mr. Spring'},
+    {id: 17, name: 'Mr. Bitch'},
+    {id: 18, name: 'Mr. Funk you!'},
+    ];
